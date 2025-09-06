b4dd1bcf18fc83b4ffffa1903147c5d2c6c6acca
opel-astra-g <kis.csaba.d@gmail.com>
2025-09-06 13:23:27 +0200

    feladatok

---
+++ elagazasok_gyakorlasa/8.py
@@ -0,0 +1,12 @@
+# A program kérjen be egy évszámot a felhasználótól! Ha ez 1900 és 2099 közé esik, akkor a program írja ki, hogy az adott évben melyik napra esik húsvét vasárnap - a Gergely naptár szerint! A kiszámítás algoritmusa (Gauss módszer): https://hu.wikipedia.org/wiki/H%C3%BAsv%C3%A9tsz%C3%A1m%C3%ADt%C3%A1s#Gauss_m%C3%B3dszere
+
+
+evszam = int(input("Írd be az évszámot: "))
+
+if 1900 <= evszam <= 2099:
+    a = evszam % 19
+    b = evszam % 4
+    c = evszam % 7
+
+    d = (19 * a + 24) % 30
+    e = (2 * b + 4 * c + 6 * d + 5) % 7
