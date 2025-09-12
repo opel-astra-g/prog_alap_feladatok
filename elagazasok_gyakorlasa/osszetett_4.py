f7240c44989391d3e6c0e1aeee4fed9ca4f8d8e1
opel-astra-g <kis.csaba.d@gmail.com>
2025-09-12 10:04:18 +0200

    osszetett 4 szerkesztes

--- elagazasok_gyakorlasa/osszetett_4.py
+++ elagazasok_gyakorlasa/osszetett_4.py
@@ -3,16 +3,16 @@ a, b = input("Írd be az A és B értéket (pl: 5 4)\n>").split()
 terulet = int(a) * int(b)
 kerulet = 2*(int(a) + int(b))
 
-atlag = 0
+atlag = False
 
 if terulet < 100 and kerulet < 100:
     print("Kis téglalap")
-    atlag += 1
+    atlag = True
 if int(a) == int(b):
     print("A téglalap egy négyzet")
-    atlag += 1
+    atlag = True
 if int(a) < int(b)/3 or int(b) < int(a)/3:
     print("A téglalap keskeny")
-    atlag += 1
+    atlag = True
 if atlag == 0:
     print("Ez egy normális téglalap")
